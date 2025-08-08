2ebb9ab2dbda2b0141c51b7294e3b09552b88900
dot-ma801 <dot.ma801@gmail.com>
2025-08-09 00:50:58 +0900

    [change] ファイル名の変更

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@ const router = createRouter({
   routes: [
     {
       path: '',
-      component: () => import('@/views/TextHighlighter.vue'),
+      component: () => import('@/views/TextHighlighterView.vue'),
     },
   ],
 })
